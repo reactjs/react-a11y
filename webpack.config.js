6f38fe87956d19e75a78ab1e5025a41add64dce9
Todd Kloots <kloots@twitter.com>
2015-06-09 22:49:14 -0700

    [Added] Ability to exclude tests (resolves #57)

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader?harmony' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' }
     ]
   }
 
