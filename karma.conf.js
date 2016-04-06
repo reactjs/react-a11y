be539b81ada116d1d5083eac6b1d52725255ac16
Romeo Van Snick <romeovs@gmail.com>
2016-04-06 12:20:29 +0200

    prepare tests for es6

--- karma.conf.js
+++ karma.conf.js
@@ -1,43 +1,42 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
 
 module.exports = function (config) {
   config.set({
-
-    browserNoActivityTimeout: 30000,
-
-    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
-
-    singleRun: process.env.CONTINUOUS_INTEGRATION === 'true',
-
-    frameworks: [ 'mocha' ],
-
-    files: [
-      'tests.webpack.js'
-    ],
-
-    preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
-    },
-
-    reporters: [ 'dots' ],
-
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-      },
-      plugins: [
+    browserNoActivityTimeout: 30000
+  , browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ]
+  , singleRun: process.env.CONTINUOUS_INTEGRATION === 'true'
+  , frameworks: [ 'mocha' ]
+  , plugins: [
+      'karma-mocha'
+    , 'karma-chrome-launcher'
+    , 'karma-firefox-launcher'
+    , 'karma-webpack'
+    , 'karma-sourcemap-loader'
+    ]
+  , files: [
+      'lib/__tests__/*.js'
+    ]
+  , preprocessors: {
+      'lib/__tests__/*.js': [ 'webpack', 'sourcemap' ]
+    }
+  // , reporters: [ 'dots' ]
+  , webpack: {
+      devtool: 'inline-source-map'
+    , module: {
+        loaders: [{
+          test: /\.js$/
+        , exclude: /node_modules/
+        , loader: 'babel-loader'
+        }]
+      }
+    , plugins: [
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('test')
         })
       ]
-    },
-
-    webpackServer: {
+    }
+  , webpackMiddleware: {
       noInfo: true
     }
-
-  });
-};
+  })
+}
