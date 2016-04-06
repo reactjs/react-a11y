62dfd6022168d0f4fedf640d6ea4c54579bbb120
Romeo Van Snick <romeovs@gmail.com>
2016-04-06 12:39:02 +0200

    clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-var webpack = require('webpack');
+var webpack = require('webpack')
 
 var plugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
   })
-];
+]
 
 if (process.env.COMPRESS) {
   plugins.push(
@@ -13,30 +13,29 @@ if (process.env.COMPRESS) {
         warnings: false
       }
     })
-  );
+  )
 }
 
 module.exports = {
-
-  output: {
-    library: 'ReactJS.A11y',
-    libraryTarget: 'var'
-  },
-
-  externals: process.env.NODE_DIST ? {} : {
-    react: 'React'
-  },
-
-  node: {
-    buffer: false
-  },
-
-  plugins: plugins,
-
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
+  entry: './lib/index.js'
+, output: {
+    filename: 'react-a11y.js'
+  , path: './dist'
+  , library: 'A11y'
+  , libraryTarget: 'var'
   }
-
-};
+, externals: process.env.NODE_DIST ? {} : {
+    react: 'React'
+  }
+, node: {
+    buffer: false
+  }
+, plugins: plugins
+, module: {
+    loaders: [{
+      test: /\.js$/
+    , exclude: /node_modules/
+    , loader: 'babel-loader'
+    }]
+  }
+}
