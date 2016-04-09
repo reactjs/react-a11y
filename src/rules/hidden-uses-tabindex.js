228484f950f825152d3b7e1dbac107ed251a791e
Romeo Van Snick <romeovs@gmail.com>
2016-04-09 21:48:52 +0200

    implement hidden-uses-tabindex

---
+++ src/rules/hidden-uses-tabindex.js
@@ -0,0 +1,21 @@
+import {
+  hiddenFromAT
+, devices
+} from '../util'
+
+export default ctx => function (tagName, props, children) {
+  const hidden   = hiddenFromAT(props)
+  const tabIndex = props.tabIndex === -1
+
+  if ( hidden && !tabIndex ) {
+    ctx.report({
+      msg: 'You have `aria-hidden="true"` applied to an interactive element but have not '
+         + 'removed it from the tab flow. This could result in a hidden tab stop for '
+         + 'users of screen readers.'
+    , url: 'http://john.foliot.ca/aria-hidden'
+    , affects: [
+        devices.screenReaders
+      ]
+    })
+  }
+}
