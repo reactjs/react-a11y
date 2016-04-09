adea90c38e752e1a101833f6f37a5f540d7f07e7
Romeo Van Snick <romeovs@gmail.com>
2016-04-09 16:09:51 +0200

    implement button-role-space

---
+++ src/rules/button-role-space.js
@@ -0,0 +1,27 @@
+import {
+  isInteractive
+, hiddenFromAT
+, listensTo
+, devices
+} from '../util'
+
+export default ctx => function (tagName, props, children) {
+  const hidden      = hiddenFromAT(props)
+  const button      = props.role === 'button'
+  const onKeyDown   = listensTo(props, 'onKeyDown')
+
+  // rule fails when the element is not hidden from aria,
+  // has role="button" and no onKeyDownHandler
+  const bad = !hidden && button && !onKeyDown
+
+  if ( bad ) {
+    ctx.report({
+      msg: 'You have `role="button"` but did not define an `onKeyDown` handler. '
+         + 'Add it, and have the "Space" key do the same thing as an `onClick` handler.'
+    , url: 'https://www.w3.org/WAI/GL/wiki/Making_actions_keyboard_accessible_by_using_keyboard_event_handlers_with_WAI-ARIA_controls'
+    , affects: [
+        devices.keyboardOnly
+      ]
+    })
+  }
+}
--- src/rules/index.js
+++ src/rules/index.js
@@ -3,6 +3,7 @@ import labelUsesFor        from './label-uses-for'
 import mouseEvents         from './mouse-events-map-to-key-events'
 import onclickUsesRole     from './onclick-uses-role'
 import onclickUsesTabIndex from './onclick-uses-tabindex'
+import buttonRoleSpace     from './button-role-space'
 
 export default {
   'img-uses-alt': imgUsesAlt
@@ -10,4 +11,5 @@ export default {
 , 'mouse-events-map-to-key-events': mouseEvents
 , 'onclick-uses-role': onclickUsesRole
 , 'onclick-uses-tabindex': onclickUsesTabIndex
+, 'button-role-space': buttonRoleSpace
 }
