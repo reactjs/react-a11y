f743b4cf2442fbfa39540c74678dfa14ba7e1ae6
Romeo Van Snick <romeovs@gmail.com>
2016-04-09 14:47:18 +0200

    clean up util module and add some utilities

---
+++ src/util/hidden-from-at.js
@@ -0,0 +1,4 @@
+
+export default function (props) {
+  return props.ariaHidden === 'true' || props.ariaHidden === true
+}
--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,8 @@
+export isInteractive from './is-interactive'
+export hiddenFromAT  from './hidden-from-at'
 
-export default {
-  devices: {
-    screenReaders: Symbol('screenReaders')
-  , keyboardOnly:  Symbol('keyboardOnly')
-  , mobile:        Symbol('mobile')
-  }
+export const devices = {
+  screenReaders: Symbol('screenReaders')
+, keyboardOnly:  Symbol('keyboardOnly')
+, mobile:        Symbol('mobile')
 }
