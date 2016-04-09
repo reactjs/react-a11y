d44ceaada9cc17e89a64cd74c77372e222725f4d
Romeo Van Snick <romeovs@gmail.com>
2016-04-09 21:17:33 +0200

    add util to check if prop is to be considered true

--- src/util/index.js
+++ src/util/index.js
@@ -1,6 +1,7 @@
 export isInteractive from './is-interactive'
 export hiddenFromAT  from './hidden-from-at'
 export listensTo     from './listens-to'
+export trueish       from './trueish'
 
 export const devices = {
   screenReaders: Symbol('screenReaders')
