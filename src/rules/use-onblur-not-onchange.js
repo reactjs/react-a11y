497d78b204d483220967cffea7b0b433b18f63eb
Romeo Van Snick <romeovs@gmail.com>
2016-04-09 21:17:44 +0200

    use trueish

--- src/rules/use-onblur-not-onchange.js
+++ src/rules/use-onblur-not-onchange.js
@@ -2,12 +2,13 @@ import {
   hiddenFromAT
 , listensTo
 , devices
+, trueish
 } from '../util'
 
 export default ctx => function (tagName, props, children) {
   const hidden   = hiddenFromAT(props)
-  const disabled = props.ariaDisabled === true
-  const readOnly = props.ariaReadonly === true
+  const disabled = trueish(props, 'ariaDisabled')
+  const readOnly = trueish(props, 'ariaReadonly')
   const onChange = listensTo(props, 'onChange')
 
   if ( !(hidden || disabled || readOnly)&& onChange ) {
