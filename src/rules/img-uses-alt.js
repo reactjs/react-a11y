ac8dbf59eeaf91966de0984a0c7eb5f2137dd446
Romeo Van Snick <romeovs@gmail.com>
2016-04-11 13:15:11 +0200

    switch to more static rule definitions

--- src/rules/img-uses-alt.js
+++ src/rules/img-uses-alt.js
@@ -1,37 +1,70 @@
+import React from 'react'
 import {
   devices
 , hiddenFromAT
 } from '../util'
 
-export default ctx => ({
-  img (props) {
-    if ( hiddenFromAT(props) ) {
-      return
+// test will be run in order
+export default [
+  {
+    tagName: 'img'
+  , msg: 'The img does not have an `alt` prop, screen-readers will not know what it is'
+  , url: 'https://dev.w3.org/html5/alt-techniques'
+  , test (tagName, props) {
+      const hidden = hiddenFromAT(props)
+      const alt    = typeof props.alt !== 'string'
+
+      return !hidden && !alt
     }
+  , affects: [
+      devices.screenReaders
+    ]
+  },
 
-    if ( typeof props.alt !== 'string' ) {
-      // no alt is present
+  {
+    tagName: 'img'
+  , msg: 'The `alt` prop cannot be empty string if role="presentation" is not set.'
+  , url: 'https://www.w3.org/TR/wai-aria/roles#presentation'
+  , test (tagName, props) {
+      const hidden = hiddenFromAT(props)
+      const alt    = typeof props.alt === 'string'
+      const empty  = alt && props.alt === ''
+      const pres   = props.role === 'presentation'
 
-      ctx.report({
-        msg: 'The img does not have an `alt` prop, '
-           + 'screen-readers will not know what it is'
-      , url: 'https://dev.w3.org/html5/alt-techniques'
-      , affects: [
-          devices.screenReaders
-        ]
-      })
-
-    } else if ( props.alt === '' && props.role !== 'presentation' ) {
-      // alt is empty and role="presentation" is not set
-
-      ctx.report({
-        msg: 'The `alt` prop cannot be empty string if '
-           + 'role="presentation" is not set.'
-      , url: 'https://www.w3.org/TR/wai-aria/roles#presentation'
-      , affects: [
-          devices.screenReaders
-        ]
-      })
+      return !hidden && alt && empty && pres
     }
+  , affects: [
+      devices.screenReaders
+    ]
   }
-})
+]
+
+export const pass = [
+  {
+    when: 'the img has an `alt`'
+  , render() { return <img src='foo' alt='nice' /> }
+  }
+, {
+    when: 'the img has an empty `alt` and role="presentation"'
+  , render() { return <img src='foo' alt='' role='presentation'/> }
+  }
+, {
+    when: 'the img is aria-hidden'
+  , render() { return <img src='foo' aria-hidden /> }
+  }
+]
+
+export const fail = [
+  {
+    when: 'the img doen\'t have an `alt`'
+  , render: () => <img src='foo' />
+  }
+, {
+    when: 'the img has alt="" but no role="presentation"'
+  , render: () => <img src='foo' alt='' />
+  }
+]
+
+export const description = `
+This is nice --
+`
