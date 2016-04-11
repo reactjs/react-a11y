9f6c1cfb597d1c498ddd651de894adfb75f5f26e
Romeo Van Snick <romeovs@gmail.com>
2016-04-11 22:26:51 +0200

    convert hidden-uses-tabindex

--- src/rules/hidden-uses-tabindex.js
+++ src/rules/hidden-uses-tabindex.js
@@ -1,22 +1,45 @@
+import React from 'react'
 import {
   hiddenFromAT
 , devices
+, isInteractive
 } from '../util'
 
-export default ctx => function (tagName, props) {
-  const hidden   = hiddenFromAT(props)
-  const tabIndex = props.tabIndex === -1
 
-  if ( hidden && !tabIndex ) {
-    ctx.report({
-      msg: 'You have `aria-hidden="true"` applied to an interactive element but have not '
-         + 'removed it from the tab flow. This could result in a hidden tab stop for '
-         + 'users of screen readers.'
-    , url: 'http://john.foliot.ca/aria-hidden'
-    , affects: [
-        devices.keyboardOnly
-      , devices.screenReaders
-      ]
-    })
+export default [{
+  msg: 'You have `aria-hidden="true"` applied to an interactive element but have not '
+      + 'removed it from the tab flow. This could result in a hidden tab stop for '
+      + 'users of screen readers.'
+, url: 'http://john.foliot.ca/aria-hidden'
+, affects: [
+    devices.keyboardOnly
+  , devices.screenReaders
+  ]
+, test(tagName, props) {
+    const hidden      = hiddenFromAT(props)
+    const interactive = isInteractive(tagName, props)
+    const tabIndex    = props.tabIndex === -1
+
+    return !hidden || !interactive || tabIndex
   }
-}
+}]
+
+
+export const pass = [{
+  when: 'an interactive element is aria-hidden and has tabindex="-1"'
+, render: () => <input aria-hidden={true} tabIndex={-1} />
+}, {
+  when: 'the element is not interactive'
+, render: () => <div aria-hidden={true} />
+}, {
+  when: 'an interactive element is not aria-hidden'
+, render: () => <input />
+}]
+
+export const fail = [{
+  when: 'an interactive element is hidden but has no tabindex'
+, render: () => <input aria-hidden={true} />
+}, {
+  when: 'an interactive element is hidden but has a bad tabindex'
+, render: () => <input aria-hidden={true} tabIndex={2} />
+}]
