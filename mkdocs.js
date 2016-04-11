db2c462c54d1b2ddb538445cc0c665a47b4a3eed
Romeo Van Snick <romeovs@gmail.com>
2016-04-11 13:17:35 +0200

    add task to write docs

---
+++ mkdocs.js
@@ -0,0 +1,46 @@
+import ReactDOM   from 'react-dom/server'
+import rules      from './src/rules'
+import fs         from 'fs'
+import path       from 'path'
+
+
+Object.keys(rules).forEach(function (rule) {
+  const {
+    'default': defn
+  , pass = []
+  , fail = []
+  , description = ''
+  } = require('./src/rules/' + rule)
+
+  let res = `# ${rule}`
+  const line = str => res += '\n' + str
+
+  line(description)
+  line('## Passes')
+  line('These elements are passed by this rule')
+  line('```js')
+  line(pass.map(function (ok) {
+    return `// no problem when ${ok.when}\n`
+      + ReactDOM.renderToStaticMarkup(ok.render())
+  }).join('\n\n'))
+  line('```')
+
+  line('## Fails')
+  line('These elements are *not* passed by this rule')
+  line('```js')
+  line(fail.map(function (ok) {
+    return `// warns when ${ok.when}\n`
+      + ReactDOM.renderToStaticMarkup(ok.render())
+  }).join('\n\n'))
+  line('```')
+  const file = path.resolve('.', 'docs', 'rules', `${rule}.md`)
+
+  fs.writeFile(file, res, function (err) {
+    if ( err ) {
+      console.log(err)
+      process.exit(1)
+    } else {
+      console.log(`written ${file}`)
+    }
+  })
+})
