097aedd92c8574e054f6d2f98fefc99cf68e8b28
Romeo Van Snick <romeovs@gmail.com>
2016-04-15 23:49:00 +0200

    valid-aria-role

--- src/rules/index.js
+++ src/rules/index.js
@@ -9,7 +9,7 @@ import noAccessKey         from './no-access-key'
 import noHashRef           from './no-hash-ref'
 import redundantAlt        from './redundant-alt'
 import onBlurNotOnChange   from './use-onblur-not-onchange'
-// import validAriaRole       from './valid-aria-role'
+import validAriaRole       from './valid-aria-role'
 // import tabIndexUsesButton  from './tabindex-uses-button'
 
 export default {
@@ -24,6 +24,6 @@ export default {
 , 'no-hash-ref': noHashRef
 , 'redundant-alt': redundantAlt
 , 'use-onblur-not-onchange': onBlurNotOnChange
-// , 'valid-aria-role': validAriaRole
+, 'valid-aria-role': validAriaRole
 // , 'tabindex-uses-button': tabIndexUsesButton
 }
--- src/rules/valid-aria-role.js
+++ src/rules/valid-aria-role.js
@@ -1,3 +1,7 @@
+import {
+  hasProp
+} from '../util'
+
 const roles = [
   'alert', 'alertdialog', 'application', 'article'
 , 'banner', 'button', 'checkbox', 'columnheader'
@@ -16,14 +20,37 @@ const roles = [
 , 'tree', 'treegrid', 'treeitem'
 ]
 
-export default ctx => function (tagName, props) {
-  const role = props.role
-
-  if ( typeof role !== 'undefined' && role !== null && roles.indexOf(role) === -1 ) {
-    ctx.report({
-      msg: 'Elements with ARIA roles must use a valid, non-abstract ARIA role.'
-    , url: 'https://www.w3.org/TR/wai-aria/roles'
-    })
+export default [{
+  msg: 'Elements with ARIA roles must use a valid, non-abstract ARIA role.'
+, url: 'https://www.w3.org/TR/wai-aria/roles'
+, audit: 'AX_ARIA_01'
+, test (tagName, props) {
+    const role = hasProp(props, 'role')
+    return !role || roles.indexOf(props.role) >= 0
   }
-}
+}]
 
+
+export const fail = [{
+  when: 'there is an invalid `role`'
+, render: React => <div role='foo' />
+}]
+
+export const pass = [{
+  when: 'there is a role and it is valid'
+, render: React => <div role='button' />
+}, {
+  when: 'there is no `role`'
+, render: React => <div />
+}]
+
+export const description = `
+The ARIA roles model requires that elements with a role attribute use a valid,
+non-abstract ARIA role. Each non-abstract ARIA role is mapped on to a known set
+of behavior by the user agent or assistive technology, so using an unknown role
+will result in the desired behavior not being available to the user.
+
+You can find a list of valid ARIA roles, along with descriptions and information
+on additional required attributes, on the
+[WAI-ARIA](http://www.w3.org/TR/wai-aria/roles#roles_categorization) site.
+`
