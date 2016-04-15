f2dc9278fcaec27e9776b4723bf3a90f3b51559a
Romeo Van Snick <romeovs@gmail.com>
2016-04-15 22:54:23 +0200

    convert no-access-key

--- src/rules/index.js
+++ src/rules/index.js
@@ -5,7 +5,7 @@ import labelUsesFor        from './label-uses-for'
 import mouseEvents         from './mouse-events-map-to-key-events'
 import onclickUsesRole     from './onclick-uses-role'
 import onclickUsesTabIndex from './onclick-uses-tabindex'
-// import noAccessKey         from './no-access-key'
+import noAccessKey         from './no-access-key'
 // import noHashRef           from './no-hash-ref'
 // import redundantAlt        from './redundant-alt'
 // import onBlurNotOnChange   from './use-onblur-not-onchange'
@@ -20,7 +20,7 @@ export default {
 , 'mouse-events-map-to-key-events': mouseEvents
 , 'onclick-uses-role': onclickUsesRole
 , 'onclick-uses-tabindex': onclickUsesTabIndex
-// , 'no-access-key': noAccessKey
+, 'no-access-key': noAccessKey
 // , 'no-hash-ref': noHashRef
 // , 'redundant-alt': redundantAlt
 // , 'use-onblur-not-onchange': onBlurNotOnChange
--- src/rules/no-access-key.js
+++ src/rules/no-access-key.js
@@ -1,18 +1,30 @@
 import {
   devices
+, hasProp
 } from '../util'
 
-export default ctx => function (tagName, props) {
-  if ( 'accessKey' in props ) {
-    ctx.report({
-      msg: 'No `accessKey` attribute allowed. Inconsistencies '
-         + 'between keyboard shortcuts and keyboard comments used by screenreader '
-         + 'and keyboard only users create a11y complications.'
-    , url: 'http://webaim.org/techniques/keyboard/accesskey#spec'
-    , affects: [
-        devices.screenReader
-      , devices.keyboardOnly
-      ]
-    })
+export default [{
+  msg: 'No `accessKey` attribute allowed. Inconsistencies '
+     + 'between keyboard shortcuts and keyboard comments used by screenreader '
+     + 'and keyboard only users create a11y complications.'
+, url: 'http://webaim.org/techniques/keyboard/accesskey#spec'
+, affects: [
+    devices.screenReader
+  , devices.keyboardOnly
+  ]
+, test (tagName, props) {
+    const accessKey = hasProp(props, 'accessKey')
+    return !accessKey
   }
-}
+}]
+
+
+export const fail = [{
+  when: 'there is an `accessKey` prop'
+, render: React => <div accessKey='a' />
+}]
+
+export const pass = [{
+  when: 'there is an no `accessKey` prop'
+, render: React => <div />
+}]
