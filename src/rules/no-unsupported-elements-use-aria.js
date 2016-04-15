12fa82fa24443c3ae28f8155b0b920c50b7d320f
Romeo Van Snick <romeovs@gmail.com>
2016-04-16 00:29:09 +0200

    implement no-unsuppoerted-elements-use-aria

---
+++ src/rules/no-unsupported-elements-use-aria.js
@@ -0,0 +1,36 @@
+import {
+  DOM
+, aria
+} from '../util'
+
+
+export default [{
+  msg: 'This element does not support ARIA roles, states and properties.'
+, AX: 'AX_ARIA_12'
+, test (tagName, props) {
+    const reserved = DOM[tagName].reserved || false
+    const hasProp  = hasProp(props, Object.keys(aria).concat('role'))
+
+    return !reserved || !hasProp
+  }
+}]
+
+export const description = `
+Certain reserved DOM elements do not support ARIA roles, states and properties.
+This is often because they are not visible, for example \`meta\`, \`html\`, \`script\`,
+\`style\`. This rule enforces that these DOM elements do not contain the \`role\` and/or
+\`aria-*\` props.
+`
+
+const fail = [{
+  when: 'the element should not be given any ARIA attributes'
+, render: React => <meta charset="UTF-8" aria-hidden="false" />
+}]
+
+const pass = [{
+  when: 'the reserver element is not given an illegal prop'
+, render: React => <meta charset="UTF-8" />
+}, {
+  when: 'an illegal props is given to a non-reserved elemeent'
+, render: React => <div aria-hidden={true} />
+}]
