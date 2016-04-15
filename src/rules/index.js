74afae0e3a8dafac04e3c7a7675e8de0948015fd
Romeo Van Snick <romeovs@gmail.com>
2016-04-15 23:32:15 +0200

    convert redundant-alt

--- src/rules/index.js
+++ src/rules/index.js
@@ -7,7 +7,7 @@ import onclickUsesRole     from './onclick-uses-role'
 import onclickUsesTabIndex from './onclick-uses-tabindex'
 import noAccessKey         from './no-access-key'
 import noHashRef           from './no-hash-ref'
-// import redundantAlt        from './redundant-alt'
+import redundantAlt        from './redundant-alt'
 // import onBlurNotOnChange   from './use-onblur-not-onchange'
 // import validAriaRole       from './valid-aria-role'
 // import tabIndexUsesButton  from './tabindex-uses-button'
@@ -22,7 +22,7 @@ export default {
 , 'onclick-uses-tabindex': onclickUsesTabIndex
 , 'no-access-key': noAccessKey
 , 'no-hash-ref': noHashRef
-// , 'redundant-alt': redundantAlt
+, 'redundant-alt': redundantAlt
 // , 'use-onblur-not-onchange': onBlurNotOnChange
 // , 'valid-aria-role': validAriaRole
 // , 'tabindex-uses-button': tabIndexUsesButton
