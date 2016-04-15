2400444448ccdf90c3a0cb3ef74c981586ca1a33
Romeo Van Snick <romeovs@gmail.com>
2016-04-15 22:48:56 +0200

    convert onclick-uses-tabindex

--- src/rules/onclick-uses-tabindex.js
+++ src/rules/onclick-uses-tabindex.js
@@ -3,26 +3,39 @@ import {
 , hiddenFromAT
 , listensTo
 , devices
+, fn
 } from '../util'
 
-export default ctx => function (tagName, props) {
-  const hidden      = hiddenFromAT(props)
-  const interactive = isInteractive(tagName, props)
-  const onClick     = listensTo(props, 'onClick')
-  const tabIndex    = props.tabIndex !== undefined
+export default [{
+  msg: 'You have a click handler on a non-interactive element but no `tabIndex` DOM property. '
+     + 'The element will not be navigable or interactive by keyboard users.'
+, url: 'http://www.w3.org/TR/wai-aria-practices/#focus_tabindex'
+, affects: [
+    devices.keyboardOnly
+  ]
+, test (tagName, props) {
+    const hidden      = hiddenFromAT(props)
+    const interactive = isInteractive(tagName, props)
+    const onClick     = listensTo(props, 'onClick')
+    const tabIndex    = props.tabIndex !== undefined
 
-  // rule fails when the element is not hidden from aria,
-  // is non-interactive, but has a click handler and no tabindex
-  const bad = !hidden && !interactive && onClick && !tabIndex
-
-  if ( bad ) {
-    ctx.report({
-      msg: 'You have a click handler on a non-interactive element but no `tabIndex` DOM property. '
-          + 'The element will not be navigable or interactive by keyboard users.'
-    , url: 'http://www.w3.org/TR/wai-aria-practices/#focus_tabindex'
-    , affects: [
-        devices.keyboardOnly
-      ]
-    })
+    return hidden || interactive || !onClick || tabIndex
   }
-}
+}]
+
+export const pass = [{
+  when: 'when there is an `onClick` with a `tabIndex`'
+, render: React => <span onClick={fn} tabIndex={0} />
+}, {
+  when: 'the element is hidden from aria'
+, render: React => <span onClick={fn} ariaHidden />
+}, {
+  when: 'the element is interactive'
+, render: React => <button onClick={fn} />
+}]
+
+export const fail = [{
+  when: 'there is an `onClick` with no `tabIndex`'
+, render: React => <span onClick={fn} />
+}]
+
