a3a9b551fe941d6f50406fe2f6adf6320650f4f9
Romeo Van Snick <romeovs@gmail.com>
2016-04-16 10:39:55 +0200

    refactor mkdocs

--- mkdocs.js
+++ mkdocs.js
@@ -3,6 +3,59 @@ import ReactDOM   from 'react-dom/server'
 import rules      from './src/rules'
 import fs         from 'fs'
 import path       from 'path'
+import URL        from 'url'
+
+const host = function (url) {
+  return URL.parse(url).host.replace(/^www\./, '')
+}
+
+const renderOptions = function (options) {
+  if ( options.length === 0 ) {
+    return '*This rule takes no options*'
+  } else {
+    return [
+      'This rule takes the following options:'
+    , ...options.map(function (opt, i) {
+        return [
+          `  ${i + 1}.  ${opt.description} (**${opt.type || "any"}**)`
+        , `      default: \`${JSON.stringify(opt.def)}\``
+        ].join('\n')
+      })
+    ].join('\n')
+  }
+}
+
+const renderExamples = function (examples, result) {
+  return [
+    '```js'
+  , examples.map(function (example) {
+
+    // render custom example options
+    const opts = example.opts
+      ? `\n// (using options: ${JSON.stringify([ "warn", ...example.opts ])})`
+      : ``
+
+    // render description
+    return `// ${result} when ${example.when}${opts}\n`
+         + ReactDOM.renderToStaticMarkup(example.render(React))
+    }).join('\n\n')
+  , '```'
+  ].join('\n')
+}
+
+
+const renderURLS = function (defn) {
+  return defn.map(function (def) {
+    let res = ''
+    if ( def.url ) {
+      res += ` - [This document](${def.url}) from ${host(def.url)}`
+    }
+    if ( def.AX ) {
+      res += ` - Google Audit defs [${def.AX}](https://github.com/GoogleChrome/accessibility-developer-tools/wiki/Audit-defs#${def.AX.toLowerCase()})`
+    }
+    return res
+  }).join('\n')
+}
 
 Object.keys(rules).forEach(function (rule) {
   const {
@@ -12,9 +65,6 @@ Object.keys(rules).forEach(function (rule) {
   , description
   , options = []
   } = require('./src/rules/' + rule)
-
-  let res = `# ${rule}`
-  const line = str => res += '\n' + (str || "")
 
   if ( !description ) {
     console.warn(`no description for rule \`${rule}\``)
@@ -28,54 +78,27 @@ Object.keys(rules).forEach(function (rule) {
     console.warn(`no failing test cases for rule \`${rule}\``)
   }
 
-  line()
-  line(description || "*no description*")
-  line()
+  const res =
+`# ${rule}
+${description || "*no description*"}
 
-  line('## Options')
-  line()
-  if ( options.length === 0 ) {
-    line('*This rule takes no options*')
-  } else {
-    line('This rule takes the following options')
-    options.forEach(function (opt, i) {
-      line(`  ${i + 1}.  ${opt.description} (**${opt.type || "any"}**)`)
-      line(`      default: \`${JSON.stringify(opt.def)}\``)
-    })
-  }
-  line()
+## options
 
-  line('## Passes')
-  line()
-  line('These elements are passed by this rule')
-  line('```js')
-  line(pass.map(function (ok) {
-    return `// no problem when ${ok.when}\n`
-      + ReactDOM.renderToStaticMarkup(ok.render(React))
-  }).join('\n\n'))
-  line('```')
+${renderOptions(options)}
 
-  line()
-  line('## Fails')
-  line()
-  line('These elements are *not* passed by this rule')
-  line('```js')
-  line(fail.map(function (bad) {
-    return `// warns when ${bad.when}\n`
-      + ReactDOM.renderToStaticMarkup(bad.render(React))
-  }).join('\n\n'))
-  line('```')
-  line()
-  line('## See also')
-  line()
-  defn.forEach(function (def) {
-    if ( def.url ) {
-      line(` - [This document](${def.url})`)
-    }
-    if ( def.AX ) {
-      line(` - Google Audit defs [${def.AX}](https://github.com/GoogleChrome/accessibility-developer-tools/wiki/Audit-defs#${def.AX.toLowerCase()})`)
-    }
-  })
+## Passes
+
+${renderExamples(pass, "passes")}
+
+## Fails
+
+${renderExamples(fail, "fails")}
+
+## See also
+
+${renderURLS(defn)}
+
+`
 
   const file = path.resolve('.', 'docs', 'rules', `${rule}.md`)
 
