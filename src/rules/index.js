1d2ebc9cb8916aeaf9efa41f8e93cbb11b7ceb85
Romeo Van Snick <romeovs@gmail.com>
2016-04-18 09:50:49 +0200

    don't do dynamic imports on modules as this is hard for packagers

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,22 +1,18 @@
-import {
-  importRules
-} from '../util'
-
-export default importRules(__dirname, [
-  'button-role-space'
-, 'hidden-uses-tabindex'
-, 'img-uses-alt'
-, 'label-uses-for'
-, 'mouse-events-map-to-key-events'
-, 'onclick-uses-role'
-, 'onclick-uses-tabindex'
-, 'no-access-key'
-, 'no-hash-ref'
-, 'redundant-alt'
-, 'use-onblur-not-onchange'
-, 'valid-aria-role'
-, 'tabindex-uses-button'
-, 'no-unsupported-elements-use-aria'
-, 'avoid-positive-tabindex'
-])
+export default {
+  'button-role-space': require('./button-role-space').default
+, 'hidden-uses-tabindex': require('./hidden-uses-tabindex').default
+, 'img-uses-alt': require('./img-uses-alt').default
+, 'label-uses-for': require('./label-uses-for').default
+, 'mouse-events-map-to-key-events': require('./mouse-events-map-to-key-events').default
+, 'onclick-uses-role': require('./onclick-uses-role').default
+, 'onclick-uses-tabindex': require('./onclick-uses-tabindex').default
+, 'no-access-key': require('./no-access-key').default
+, 'no-hash-ref': require('./no-hash-ref').default
+, 'redundant-alt': require('./redundant-alt').default
+, 'use-onblur-not-onchange': require('./use-onblur-not-onchange').default
+, 'valid-aria-role': require('./valid-aria-role').default
+, 'tabindex-uses-button': require('./tabindex-uses-button').default
+, 'no-unsupported-elements-use-aria': require('./no-unsupported-elements-use-aria').default
+, 'avoid-positive-tabindex': require('./avoid-positive-tabindex').default
+}
 
