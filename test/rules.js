9e43ec32185ed528a047131dbf9ba6f6d9573e23
Romeo Van Snick <romeovs@gmail.com>
2016-04-24 20:07:56 +0200

    remove test-rules from source tree, so it doen't show warnings and need chai

--- test/rules.js
+++ test/rules.js
@@ -3,9 +3,9 @@ import ReactDOM   from 'react-dom'
 import path       from 'path'
 
 import rules      from '../src/rules'
-import {
-  testRules
-} from '../src/util'
+import testRules  from '../src/util/test-rules'
+
+// const ctx = require.context('../src/rules', true, /\.js$/)
 
 testRules({
   React
