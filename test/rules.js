286907ad0e645ec59b3c6dc60cb7f1c582a67539
Erin Doyle <edoyle@cashstar.com>
2017-05-01 09:04:59 -0400

    Updated the the karma dependencies and made some changes to the karma config

--- test/rules.js
+++ test/rules.js
@@ -1,16 +1,16 @@
-import React      from 'react'
-import ReactDOM   from 'react-dom'
-import path       from 'path'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import path from 'path';
 
-import rules      from '../src/rules'
-import testRules  from '../src/util/test-rules'
+import rules from '../src/rules';
+import testRules from '../src/util/test-rules';
 
 // const ctx = require.context('../src/rules', true, /\.js$/)
 
 testRules({
-  React
-, ReactDOM
-, ruleDir: path.resolve(__dirname, '..', 'src', 'rules')
-, rules
-})
+    React,
+    ReactDOM,
+    ruleDir: path.resolve(__dirname, '..', 'src', 'rules'),
+    rules
+});
 
